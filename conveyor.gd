057a4bd72d0580fe7aea19f7d01cfffc34f89322
psychowolf960 <119869367+psychowolf960@users.noreply.github.com>
2025-11-15 09:25:10 +0100

    f

--- conveyor.gd
+++ conveyor.gd
@@ -1,34 +1,21 @@
 extends Node2D
 
 @export var boxes_root: Node
-@export var push_velocity := 650.0
+@export var push_velocity := 550.0
 @export var scroll_speed := 100.0
-@export var push_time := 0.8
 
 @onready var mat = $TextureRect.material
-var texture_offset := 0.0
-var pushing := false
-func _input(event):
-	if Input.is_action_just_pressed("next"):
-		next_box()
 
 var offset := 0.0
 
 func _process(delta):
-	if pushing:
+	var is_pushing = Input.is_action_pressed("next")
+	
+	for box in boxes_root.get_children():
+		if box.is_in_group("boxes"):
+			if is_pushing:
+				box.linear_velocity.x = push_velocity
+
+	if is_pushing:
 		offset = fmod(offset + scroll_speed * delta / $TextureRect.texture.get_width(), 1.0)
 		mat.set("shader_parameter/offset_x", offset)
-func next_box():
-	pushing = true
-
-	for box in boxes_root.get_children():
-		if box.is_in_group("boxes"):
-			box.linear_velocity.x = push_velocity
-
-	await get_tree().create_timer(push_time).timeout
-
-	pushing = false
-
-	for box in boxes_root.get_children():
-		if box.is_in_group("boxes"):
-			box.linear_velocity = Vector2.ZERO
